c3b324f259e4ccf57667deaf7756558aa276473d
CodeWithPawan15 <pavanborde.personal97@gmail.com>
2026-02-05 03:13:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ const noBtn = document.querySelector(".js-no-btn");
 
 // /change the postion of no button
 noBtn.addEventListener("mouseover", () => {
-  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
-  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+  //const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
+  //const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+  
+  const newX = Math.max(0, Math.floor(Math.random() * maxWidth));
+  const newY = Math.max(0, Math.floor(Math.random() * maxHeight));
 
   noBtn.style.left = `${newX}px`;
   noBtn.style.top = `${newY}px`;
